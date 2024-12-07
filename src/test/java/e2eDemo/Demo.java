d3d80b785cc3a7e83d9bdf9fb5d3cf278f7f3cdc
Sergio Villagomez Ortega <martin.v.ortega@gmail.com>
2024-12-07 16:17:18 -0500

    feat(sum): add test case 3

--- src/test/java/e2eDemo/Demo.java
+++ src/test/java/e2eDemo/Demo.java
@@ -2,6 +2,7 @@ package e2eDemo;
 
 import io.appium.java_client.AppiumBy;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
@@ -9,6 +10,7 @@ import org.testng.annotations.Test;
 import runners.BaseConfig;
 
 import java.time.Duration;
+import java.util.List;
 
 public class Demo extends BaseConfig {
 
@@ -62,6 +64,44 @@ public class Demo extends BaseConfig {
         String toastMessage = driver.findElement(AppiumBy.xpath("(//android.widget.Toast)[1]")).getAttribute("name");
         Assert.assertEquals(toastMessage, "Please enter your name");
         System.out.println(toastMessage);
+    }
+
+    @Test
+    public void addToCartProduct () throws InterruptedException {
+
+        // Inicio de session
+        driver.findElement(AppiumBy.id("com.androidsample.generalstore:id/spinnerCountry")).click();
+        String country = scrollAndClick(driver,"Argentina");
+        Assert.assertEquals(country, "Argentina");
+
+        driver.findElement(AppiumBy.id("com.androidsample.generalstore:id/nameField")).sendKeys("Elena");
+        driver.findElement(AppiumBy.id("com.androidsample.generalstore:id/radioFemale")).click();
+        driver.findElement(AppiumBy.id("com.androidsample.generalstore:id/btnLetsShop")).click();
+
+        // Agregar productos al carrito de compras
+        driver.findElements(AppiumBy.xpath("//android.widget.TextView[@text='ADD TO CART']")).get(0).click();
+        driver.findElements(AppiumBy.xpath("//android.widget.TextView[@text='ADD TO CART']")).get(0).click();
+        driver.findElement(AppiumBy.id("com.androidsample.generalstore:id/appbar_btn_cart")).click();
+
+        // Validación de sumatoria de precios de página Cart
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        wait.until(ExpectedConditions.attributeContains(driver.findElement(
+                AppiumBy.id("com.androidsample.generalstore:id/toolbar_title")), "text", "Cart"));
+
+        List<WebElement> productPrices = driver.findElements(By.id("com.androidsample.generalstore:id/productPrice"));
+        int count = productPrices.size();
+        double totalsum = 0;
+        for (int i = 0; i < count; i++) {
+            String amountPrices = productPrices.get(i).getText();
+            Double price =  Double.parseDouble(amountPrices.substring(1));
+            totalsum = totalsum + price;
+            // System.out.println("Price :" +  price);
+            System.out.printf("Suma de %d: %.2f%n", i+1, totalsum);
+        }
+        System.out.println("Total Sum: " + totalsum);
+
+        Thread.sleep(3000);
+
     }
 
 }
