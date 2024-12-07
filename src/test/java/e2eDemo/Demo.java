81d70ace73e9f430c3f8b2209f8f56d4833d6767
Sergio Villagomez Ortega <martin.v.ortega@gmail.com>
2024-12-07 14:14:36 -0500

    feat(Cart): update test case, validacion de carga de pagina cart

--- src/test/java/e2eDemo/Demo.java
+++ src/test/java/e2eDemo/Demo.java
@@ -2,16 +2,20 @@ package e2eDemo;
 
 import io.appium.java_client.AppiumBy;
 import org.openqa.selenium.By;
-import org.openqa.selenium.Dimension;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import runners.BaseConfig;
+
+import java.time.Duration;
 
 public class Demo extends BaseConfig {
 
     @Test
     public void Principal() throws InterruptedException {
 
+        // Inicio de session
         driver.findElement(AppiumBy.id("com.androidsample.generalstore:id/spinnerCountry")).click();
         String country = scrollAndClick(driver,"Argentina");
         Assert.assertEquals(country, "Argentina");
@@ -22,6 +26,7 @@ public class Demo extends BaseConfig {
 
         Thread.sleep(3000);
 
+        // Seleccionar un producto en especifico
         int productCount = driver.findElements(By.id("com.androidsample.generalstore:id/productName")).size();
 
         String productSelect = scrollAndClick(driver,"PG 3");
@@ -34,12 +39,15 @@ public class Demo extends BaseConfig {
 
         driver.findElement(AppiumBy.id("com.androidsample.generalstore:id/appbar_btn_cart")).click();
 
+        // Validacion de carga de pagina Cart
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        wait.until(ExpectedConditions.attributeContains(driver.findElement(
+                AppiumBy.id("com.androidsample.generalstore:id/toolbar_title")), "text", "Cart"));
+
+        String lastPageProduct = driver.findElement(AppiumBy.id("com.androidsample.generalstore:id/productName")).getText();
+        Assert.assertEquals(lastPageProduct, "PG 3");
+
         Thread.sleep(3000);
-
-
-
-
-
     }
 
     @Test
