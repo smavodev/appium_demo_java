e69e3c481c8fee8955947772c390f2d5a1a0201c
Sergio Villagomez Ortega <martin.v.ortega@gmail.com>
2025-01-15 17:44:54 -0500

    feat(locators) add test localizadores en IOS

--- src/test/java/appiumIos/IOSBasic.java
+++ src/test/java/appiumIos/IOSBasic.java
@@ -8,7 +8,12 @@ public class IOSBasic extends  BaseConfig{
     @Test
     public void IOSBasicTest(){
         driver.findElement(AppiumBy.xpath("//XCUIElementTypeTable/XCUIElementTypeCell[2]/XCUIElementTypeOther[1]/XCUIElementTypeOther")).click();
+        driver.findElement(AppiumBy.iOSClassChain("**/XCUIElementTypeStaticText[`name == \"Text Entry\"`]")).click();
+        driver.findElement(AppiumBy.xpath("//XCUIElementTypeTextField")).sendKeys("Demo de prueba");
+        driver.findElement(AppiumBy.accessibilityId("OK")).click();
 
+        driver.findElement(AppiumBy.iOSNsPredicateString("name == \"Confirm / Cancel\"")).click();
+        driver.findElement(AppiumBy.iOSNsPredicateString("name == \"Confirm\"")).click();
     }
 
 }
