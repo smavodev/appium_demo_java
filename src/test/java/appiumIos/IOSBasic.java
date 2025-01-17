a46346f692193940c7b31ea14f8b444489660133
Sergio Villagomez Ortega <martin.v.ortega@gmail.com>
2025-01-17 15:00:35 -0500

    feat(scroll), Add test case SCROLL and Picker

--- src/test/java/appiumIos/IOSBasic.java
+++ src/test/java/appiumIos/IOSBasic.java
@@ -1,12 +1,17 @@
 package appiumIos;
 
 import io.appium.java_client.AppiumBy;
+import io.appium.java_client.TouchAction;
+import io.appium.java_client.touch.offset.PointOption;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.RemoteWebElement;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 
 public class IOSBasic extends  BaseConfig{
 
@@ -30,8 +35,34 @@ public class IOSBasic extends  BaseConfig{
         params.put("element", ((RemoteWebElement)ele).getId());
         params.put("duration", 5);
         driver.executeScript("mobile:touchAndHold", params);
+    }
 
+    @Test
+    public void IOSScrollTest() throws InterruptedException {
 
+        Map<String,Object> params = new HashMap<>();
+        params.put("direction", "down");
+        params.put("percent", 0.25);  // Porcentaje del desplazamiento (opcional, controla la distancia)
+
+        driver.executeScript("mobile:scroll", params);
+        driver.findElement(AppiumBy.accessibilityId("Web View")).click();
+        Thread.sleep(2000);
+
+        driver.findElement(AppiumBy.xpath("//XCUIElementTypeButton[@name=\"UIKitCatalog\"]")).click();
+    }
+
+    @Test
+    public void IOSPickerTest() throws InterruptedException {
+
+        driver.findElement(AppiumBy.accessibilityId("Picker View")).click();
+        driver.findElement(AppiumBy.accessibilityId("Red color component value")).sendKeys("90");
+        driver.findElement(AppiumBy.accessibilityId("Green color component value")).sendKeys("80");
+        driver.findElement(AppiumBy.accessibilityId("Blue color component value")).sendKeys("105");
+
+        String number = driver.findElement(AppiumBy.accessibilityId("Blue color component value")).getText();
+        Assert.assertEquals(number, "105");
+        System.out.println(number);
+        Thread.sleep(2000);
     }
 
 }
